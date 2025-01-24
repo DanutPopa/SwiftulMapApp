507529c7cca405109a373ef98e9f1339b3f29524
DanutPopa <popa.ion.danut@gmail.com>
2025-01-24 15:32:45 +0200

    Initial Commit

---
+++ SwiftulMapApp/SwiftulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulMapAppApp.swift
+//  SwiftulMapApp
+//
+//  Created by Danut Popa on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
