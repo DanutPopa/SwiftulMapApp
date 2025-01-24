dc6c1101f9dc23c0f453b4c953ee8caf5fe9f26d
DanutPopa <popa.ion.danut@gmail.com>
2025-01-24 16:24:55 +0200

    Adding Location model.

---
+++ SwiftulMapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftulMapApp
+//
+//  Created by Danut Popa on 24.01.2025.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
